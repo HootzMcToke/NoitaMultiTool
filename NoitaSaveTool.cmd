8114230a70be095251c8d783e1b0294b43743d96
HootzMcToke <14063105+HootzMcToke@users.noreply.github.com>
2020-08-18 23:24:59 -0700

    Update NoitaSaveTool.cmd

--- NoitaSaveTool.cmd
+++ NoitaSaveTool.cmd
@@ -77,7 +77,7 @@ GOTO backupmain
 cls
 ECHO Backing Up Save...
 if not exist "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00A\" mkdir "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00A\"
-robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00A" /e /xd "stats" >NUL
+robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00A" /e /xd "stats" "persistent" >NUL
 ECHO Backup Complete - Located in Folder save00A
 pause
 GOTO backupmain
@@ -86,7 +86,7 @@ GOTO backupmain
 cls
 ECHO Backing Up Save...
 if not exist "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00B\" mkdir "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00B\"
-robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00B" /e /xd "stats" >NUL
+robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00B" /e /xd "stats" "persistent" >NUL
 ECHO Backup Complete - Located in Folder save00B
 pause
 GOTO backupmain
@@ -95,7 +95,7 @@ GOTO backupmain
 cls
 ECHO Backing Up Save...
 if not exist "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00C\" mkdir "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00C\"
-robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00C" /e /xd "stats" >NUL
+robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00C" /e /xd "stats" "persistent" >NUL
 ECHO Backup Complete - Located in Folder save00C
 pause
 GOTO backupmain
@@ -112,11 +112,11 @@ goto backupmain
 cls
 ECHO Overwriting all Slots with current Game Save Data
 if not exist "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00A\" mkdir "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00A\"
-robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00A" /e /xd "stats" >NUL
+robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00A" /e /xd "stats" "persistent" >NUL
 if not exist "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00B\" mkdir "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00B\"
-robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00B" /e /xd "stats" >NUL
+robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00B" /e /xd "stats" "persistent" >NUL
 if not exist "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00C\" mkdir "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00C\"
-robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00C" /e /xd "stats" >NUL
+robocopy "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00" "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita\save00C" /e /xd "stats" "persistent" >NUL
 ECHO Overwrite Complete - All Slots are based on Last Game Save!
 pause 
 GOTO backupmain
@@ -266,13 +266,85 @@ IF ERRORLEVEL 3 GOTO SeeSeed
 IF ERRORLEVEL 2 GOTO SetSeed
 IF ERRORLEVEL 1 GOTO RandomSeed
 
+:SetSeed
+::Find and Replace script allows the user to 
+::define a file path, file name and a string 
+::to find and replace so as to create a new file.
+::
+::Original file is backed up with an added extension of *.bak, in case
+::the user finds the need to go back to the original.
+
+@echo off
+::Use the path from whence the script was executed as
+::the Current Working Directory
+set CWD=%~dp0
+
+::***BEGIN MODIFY BLOCK***
+::The variables below should be modified to the
+::files to be changed and the strings to find/replace
+::Include trailing backslash in _FilePath
+set _FilePath=Path\To\File\
+set _FileName=FileNameToModify
+::_WrkFile is the file on which the script will make
+::modifications.
+set _WrkFile=BackupFileName
+set OldStr="The string to be found and replaced, enclosed in double-quotes"
+set NewStr="The new string to replace the value of OldStr, enclosed in double-quotes"
+::***END MODIFY BLOCK***
+
+::Set a variable which is used by the
+::search and replace section to let us
+::know if the string to be modified was
+::found or not.
+set _Found=Not found
+
+SETLOCAL
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+if not exist "%_FilePath%%_FileName%" goto :NotFound
+
+::If a backup file exists, delete it
+if exist "%_FilePath%%_WrkFile%" (
+    echo Deleting "%_FilePath%%_WrkFile%" 
+    del "%_FilePath%%_WrkFile%" >nul 2>&1
+    )
+
+echo.
+echo Backing up "%_FilePath%%_FileName%"...
+copy "%_FilePath%%_FileName%" "%_FilePath%%_WrkFile%" /v
+
+::Delete the original file. No worries, we got a backup.
+if exist "%_FilePath%%_FileName%" del "%_FilePath%%_FileName%"
+echo.
+echo Searching for %OldStr% string...
+echo.
+for /f "usebackq tokens=*" %%a in ("%_FilePath%%_WrkFile%") do (
+    set _LineChk=%%a
+    if "!_LineChk!"==%OldStr% (
+        SET _Found=Found 
+        SET NewStr=!NewStr:^"=! 
+        echo !NewStr!
+        ) else (echo %%a)
+        )>>"%_FilePath%%_FileName%" 2>&1
+
+::If we didn't find the string, rename the backup file to the original file name
+::Otherwise, delete the _WorkFile as we re-created the original file when the
+::string was found and replaced.
+if /i "!_Found!"=="Not found" (echo !_Found! && del "%_FilePath%%_FileName%" && ren "%_FilePath%%_WrkFile%" %_FileName%) else (echo !_Found! && del "%_FilePath%%_WrkFile%")
+goto :welcome
+
+:NotFound
+echo.
+echo File "%_FilePath%%_FileName%" missing. 
+echo Cannot continue...
+echo.
+:: Pause script for approx. 10 seconds...
+PING 127.0.0.1 -n 11 > NUL 2>&1
+goto :welcome
+
+
 :SeeSeed
 ECHO See Seed Coming Soon
-pause
-goto welcome
-
-:SetSeed
-ECHO Set Seed Coming Soon
 pause
 goto welcome
 
@@ -307,3 +379,5 @@ REM http://waynes-world-it.blogspot.com
 REM https://jrgraphix.net/r/Unicode/2500-257F
 REM http://steve-jansen.github.io/guides/windows-batch-scripting/part-1-getting-started.html
 REM https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/commands-by-server-role:welcome
+REM https://superuser.com/questions/700458/looking-to-create-a-batch-file-capable-of-editing-a-line-or-word-in-a-text-fil
+REM https://stackoverflow.com/questions/34090258/find-steam-games-folder
